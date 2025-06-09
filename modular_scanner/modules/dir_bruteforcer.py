4f1fc29d6e1a2e0774cbc9ba0712f562bfdf03bb
student283803 <283803@student.pwr.edu.pl>
2025-06-09 13:20:59 +0200

    aktualizacja 9.06.25

---
+++ modular_scanner/modules/dir_bruteforcer.py
@@ -0,0 +1,3 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from
+from tqdm import tqdm
---
+++ modular_scanner/utils/reporter.py
@@ -0,0 +1,36 @@
+import os
+import json
+from datetime import datetime
+from urllib.parse import urlparse
+import threading
+
+class Reporter:
+    def __init__(self, base_dir: str = 'archive'):
+        self.lock = threading.Lock()
+
+    def setup_target(self, target_url: str):
+        """creating unique catalogue for each target"""
+
+        parsed_url = urlparse(target_url)
+        hostname = parsed_url.hostname or "local target"
+        timestamp = datetime.utcnow().strftime("%Y%-m%-d%-H%-M%-S")
+
+        self.report_dir = os.path.join('archive', f'{hostname}_{timestamp}')
+
+        try:
+            os.makedirs(self.report_dir, exist_ok=True)
+            print(f"Results will be stored in {self.report_dir}")
+        except OSError as e:
+            print(f"Failed to create directory {self.report_dir}: {e}")
+            self.report_dir = None
+
+    def log_raw(self, module_name: str, data: dict):
+        if not self.report_dir:
+            return
+        with self.lock:
+            file_path = os.path.join(self.report_dir, f'{module_name}_raw.json')
+            with open(file_path, 'a', encoding='utf-8') as f:
+                json.dump(data, f)
+                f.write('\n')
+
+
