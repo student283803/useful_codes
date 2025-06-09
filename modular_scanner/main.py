8e688862a5b011b89c3e222221ee49f5fa64067c
student283803 <283803@student.pwr.edu.pl>
2025-06-09 14:41:00 +0200

    aktualizacja 9.06.25

--- modular_scanner/main.py
+++ modular_scanner/main.py
@@ -1,18 +1,39 @@
 import argparse
 from utils.http_client import HttpClient
+from utils.reporter import Reporter
+from modules.dir_bruteforcer import DirBruteForcer
 
 def main():
     parser = argparse.ArgumentParser(description='Modular scan scanner')
     parser.add_argument('-u', '--url', required=True, help='URL to scan')
+    parser.add_argument('-m', '--module', required=True, choices=['dirscan'], help='Module to scan (for example dirscan)')
+    parser.add_argument('-w', '--wordlist', required=True, help='Path to wordlist dictionary')
     args = parser.parse_args()
     target_url = args.url
 
-    print(f"target url: {target_url}\n")
-
+    print(f"target url: {target_url} | module: {args.module}\n")
     http_client = HttpClient()
+
+    if args.module == 'dirscan' and not args.wordlist:
+        parser.error("--wordlist is needed for dirscan")
+
+    reporter = Reporter()
+    reporter.setup_target(target_url)
+
+
+    if args.module == 'dirscan':
+        bruteforcer = DirBruteForcer(http_client, target_url, reporter=reporter)
+        results = bruteforcer.scan(args.wordlist)
+        if results:
+            print(f"following resources was found: ")
+            for url, status_code in results:
+                print(f"  [+] {url} (Status: {status_code})")
+        else:
+            print(f"no hidden resources were found")
+
+
     print(f"conecting to {target_url}\n")
     response = http_client.get(target_url)
-
     if response:
         print(f"Success! Response from {target_url}: {response}")
 
--- modular_scanner/modules/dir_bruteforcer.py
+++ modular_scanner/modules/dir_bruteforcer.py
@@ -1,3 +1,64 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from
+from utils.http_client import HttpClient
+from utils.reporter import Reporter
 from tqdm import tqdm
+
+
+class DirBruteForcer:
+    def __init__(self, http_client: HttpClient, target_url: str, reporter: Reporter, max_threads: int = 20):
+        self.client = http_client
+        self.target_url = target_url if target_url.endswith('/') else target_url + '/'
+        self.reporter = reporter
+        self.max_threads = max_threads
+
+    def _check_path(self, path:str):
+        path = path.strip()
+        test_url = self.target_url + path
+
+        response = self.client.get(test_url)
+
+        if response:
+
+            raw_log_data = {
+                'path': path,
+                'url': test_url,
+                'status_code': response.status_code,
+                'content_length': len(response.text),
+                'headers': dict(response.headers)
+            }
+            self.reporter.log_raw('dir_bruteforcer', raw_log_data)
+
+            if response.status_code in [200, 204, 301, 302, 307, 401, 403]:
+                return (test_url, response.status_code)
+        return None
+
+    def scan(self, wordlist_path: str):
+        """
+        Args:
+            wordlist_path (str): path to file with dict
+
+        Returns:
+            list: tuples (url, status_code) for found resources
+        """
+
+        try:
+            with open(wordlist_path, 'r', encoding='utf-8') as f:
+                words = f.readlines()
+        except FileNotFoundError:
+            print(f"{wordlist_path} not found")
+            return []
+
+        found_resources = []
+        with ThreadPoolExecutor(max_workers=self.max_threads) as executor:
+            print(f"[*]Starting scan for {wordlist_path} with {len(words)} words")
+
+            futures = {executor.submit(self._check_path, word): word for word in words}
+            for future in tqdm(as_completed(futures), total=len(words), desc="Scanning"):
+                results = future.result()
+                if results:
+                    found_resources.append(results)
+        return found_resources
