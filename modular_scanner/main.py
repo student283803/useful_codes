7884cebc73294f3b42623824841ffad5743d3362
student283803 <283803@student.pwr.edu.pl>
2025-06-09 09:45:15 +0200

    aktualizacja 9.06.25

--- modular_scanner/main.py
+++ modular_scanner/main.py
@@ -1,3 +1,18 @@
 import argparse
 from utils.http_client import HttpClient
 
+def main():
+    parser = argparse.ArgumentParser(description='Modular scan scanner')
+    parser.add_argument('-u', '--url', required=True, help='URL to scan')
+    args = parser.parse_args()
+    target_url = args.url
+
+    print(f"target url: {target_url}\n")
+
+    http_client = HttpClient()
+    print(f"conecting to {target_url}\n")
+    response = http_client.get(target_url)
+
+    if response:
+        print(f"Success! Response from {target_url}: {response}")
+        print(f"response code {response.status_code}\n")
